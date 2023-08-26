13f8d63c1a11d49a1b432fb7ad4234e37259d153
Arun Kumar <aruncode99@gmail.com>
2023-08-26 18:00:33 +0530

    Step 3,4,5 -> Pending Status

--- Project-1/index.js
+++ Project-1/index.js
@@ -19,11 +19,34 @@ app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
+// Adding Dynamic routes
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
   res.json(user);
 });
+
+// Post Request for creating new user
+app.post("/api/users", (req, res) => {
+  res.json({ status: "pending" });
+});
+
+// Patch Request to edit the user with id
+app.patch("/api/users/:id", (req, res) => {
+  res.json({ status: "pending" });
+});
+
+// Delete Request to delete the user with id
+app.delete("/api/users/:id", (req, res) => {
+  res.json({ status: "pending" });
+});
+
+// Note: We can merge these request with same route
+// app
+//   .route("/api/users/:id")
+//   .get((req, res) => {})
+//   .patch(() => {})
+//   .delete((req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running at port : ${PORT}`);
