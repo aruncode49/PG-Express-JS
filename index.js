87ef478082dc07e3b93a998ddf3614b79d14038f
Arun Kumar <aruncode99@gmail.com>
2023-08-26 15:56:59 +0530

    adding multiple routes with ease

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/about", (req, res) => {
+  res.send("This is about page!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("This is contact page!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
