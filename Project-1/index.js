ed987547d7fa9fa6b28602434f17865a012d5bef
Arun Kumar <aruncode99@gmail.com>
2023-08-26 17:47:36 +0530

    Step-2 Add Dynamic Routes

--- Project-1/index.js
+++ Project-1/index.js
@@ -5,8 +5,24 @@ const app = express();
 const PORT = 8000;
 
 // ROUTES
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+  res.send(html);
+});
+
+// Rest Api Points
+app.get("/api/users", (req, res) => {
   res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  res.json(user);
 });
 
 app.listen(PORT, () => {
